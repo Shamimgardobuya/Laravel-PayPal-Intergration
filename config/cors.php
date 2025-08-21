69906445137a8a13e9187ef6e520ef6432a5c2ff
Shamimgardobuya <shamimobuya@gmail.com>
2025-08-21 15:26:34 +0300

    adde frontend access

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://little-lions-junior.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
